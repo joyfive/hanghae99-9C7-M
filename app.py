fe410d767b0a6e1f7b8c166393a5520e83f4efb1
damin0320 <damin0320@gmail.com>
2022-09-20 23:39:03 +0900

    ADD : 데이터 write->view로 보내주기 및 저장

--- app.py
+++ app.py
@@ -73,6 +73,22 @@ def write():
 
 @app.route("/content", methods=["POST"])
 def content():
+    album_receive = request.form["album_give"]
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    params = {'axnm': album_receive}
+    res = requests.get('https://www.genie.co.kr/detail/albumInfo', headers=headers, params=params)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    s_artist = soup.select_one('div.info-zone > ul > li:nth-child(1) > span.value > a')
+    s_img = soup.select_one('div.album-detail-infos > div.photo-zone > a > span.cover')
+    s_tit = soup.select_one('#body-content > div.album-detail-infos > div.info-zone > h2')
+
+    img = s_img.find('img')['src'][19:-19],
+    title = s_tit.text.strip(),
+    artist = s_artist.text
+
+
     content_list = list(db.musics.find({}, {'_id': False}))
     count = len(content_list) + 1
     title_receive = request.form["title_give"]
@@ -83,7 +99,10 @@ def content():
         "num" : count,
         "title": title_receive,
         "content": content_receive,
-        "star": star_receive
+        "star": star_receive,
+        'img': img[0],
+        's_title': title[0],
+        'artist': artist
     }
 
     db.musics.insert_one(doc)
