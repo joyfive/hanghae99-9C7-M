edf97bd2c50fd20207fb7c035fa50b1afabd99c4
joyfive <joy_five@kakao.com>
2022-09-20 13:54:43 +0900

    POST 로 인풋박스 데이터 끌어오기 성공! 끌어온 데이터로 크롤링은 어떻게 하지? 데이터 저장이 필수인가?

--- test01.py
+++ test01.py
@@ -1,23 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+from pymongo import MongoClient
+import certifi
 
-params = {'axnm': 81867444}
-res = requests.get('https://www.genie.co.kr/detail/albumInfo', headers=headers, params=params)
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+ca = certifi.where()
+
+jclient = MongoClient('mongodb+srv://joyfive:whdlvkdlqm999@cluster0.omhyorx.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+jdb = jclient.dbsparta
 
 # https://www.genie.co.kr/detail/songInfo?xgnm=97582507
 # https://www.genie.co.kr/detail/albumInfo?axnm=82842800
-soup = BeautifulSoup(res.text, 'html.parser')
-
-s_artist = soup.select_one('div.info-zone > ul > li:nth-child(1) > span.value > a')
-
-s_img = soup.select_one('div.album-detail-infos > div.photo-zone > a > span.cover')
-s_tit = soup.select_one('#body-content > div.album-detail-infos > div.info-zone > h2')
-
-img = s_img.find('img')['src'][2:-19]
-title = s_tit.text.strip()
-artist = s_artist.text
 
 
-print(img,title,artist)
+
+# print(img,title,artist)
+
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/album", methods=["POST"])
+def album_input():
+    album_receive = request.form["album_give"]
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    params = {'axnm': album_receive}
+    res = requests.get('https://www.genie.co.kr/detail/albumInfo', headers=headers, params=params)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    s_artist = soup.select_one('div.info-zone > ul > li:nth-child(1) > span.value > a')
+    s_img = soup.select_one('div.album-detail-infos > div.photo-zone > a > span.cover')
+    s_tit = soup.select_one('#body-content > div.album-detail-infos > div.info-zone > h2')
+
+    img = s_img.find('img')['src'][2:-19],
+    title = s_tit.text.strip(),
+    artist = s_artist.text
+
+    return jsonify({
+        'img': img,
+        'title': title,
+        'artist': artist
+    })
+# @app.route("/album", methods=["GET"])
+# def album_preview():
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5050, debug=True)
