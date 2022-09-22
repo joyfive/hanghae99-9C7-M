907ecb065b21cdc7623c4e71c496af7692bc72eb
damin <damin0320@gmail.com>
2022-09-22 17:03:59 +0900

    FIX : app.py and sign.js

--- static/sign.js
+++ static/sign.js
@@ -100,7 +100,7 @@
                 },
                 success: function (response) {
                     alert("회원가입을 축하드립니다!")
-                    window.location.replace("/sign")
+                    window.location.replace("/signin")
                 }
             });
 
